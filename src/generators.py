256501c1ac5e7c2e635d454c6afc64203bf22e37
Linar Khamidulin <hamidulin16@yandex.ru>
2024-09-21 16:40:51 +0300

    add generators.py

---
+++ src/generators.py
@@ -0,0 +1,28 @@
+def filter_by_currency(transactions: list, code: str):
+    """функция филтрует по значению "code" """
+    try:
+        filter_transactions = filter(lambda item: item["operationAmount"]["currency"]["code"] == code, transactions)
+        return filter_transactions
+    except Exception:
+        return None
+
+
+def transaction_descriptions(transactions: list) -> str:
+    """функция возвращает описание каждой операции по очереди"""
+    try:
+        description = (transaction.get("description") for transaction in transactions)
+        return description
+    except Exception:
+        return None
+
+
+def card_number_generator(start: int, end: int):
+    """функция сгенерирует номера карт и выдает их в банковском формате"""
+
+    for number in range(start, end + 1):
+        card_number_ = str(number)
+        while len(card_number_) < 16:
+            card_number_ = "0" + card_number_
+        card_number_format = f"{card_number_[:4]} {card_number_[4:8]} {card_number_[-8:-4]} {card_number_[-4:]}"
+
+        yield card_number_format
