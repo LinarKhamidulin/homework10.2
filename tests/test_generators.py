2ecd325ab80b8cd26caea6dc783cd582a0534f21
Linar Khamidulin <hamidulin16@yandex.ru>
2024-09-22 15:09:02 +0300

    add test_generators.py

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -3,8 +3,9 @@ import pytest
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 #Тест функции filter_by_currency
-
-test_transactions = [
+@pytest.fixture
+def test_transactions()-> list:
+    transactions = [
     {
         "id": 939719570,
         "state": "EXECUTED",
@@ -27,7 +28,7 @@ test_transactions = [
         "id": 873106923,
         "state": "EXECUTED",
         "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб."}},
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.","code": "RUB"}},
         "description": "Перевод со счета на счет",
         "from": "Счет 44812258784861134719",
         "to": "Счет 74489636417521191160",
@@ -45,25 +46,22 @@ test_transactions = [
         "id": 594226727,
         "state": "CANCELED",
         "date": "2018-09-12T21:27:25.241689",
-        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб."}},
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
         "description": "Перевод организации",
         "from": "Visa Platinum 1246377376343588",
         "to": "Счет 14211924144426031657",
     },
-]
+    ]
+    return transactions
 
 @pytest.fixture
 def my_code()-> str:
     return "USD"
 
-@pytest.fixture
-def my_code_not()-> str:
-    return ""
 
 def test_filter_by_currency(test_transactions: list,my_code: str):
     usd_transactions = filter_by_currency(test_transactions, my_code)
-    for _ in range(1):
-        assert next(usd_transactions) == {'id': 939719570,
+    assert next(usd_transactions) == {'id': 939719570,
                                      'state': 'EXECUTED',
                                      'date': '2018-06-30T02:08:58.425572',
                                      'operationAmount': {'amount': '9824.07','currency': {'name': 'USD', 'code': 'USD'}},
@@ -72,42 +70,29 @@ def test_filter_by_currency(test_transactions: list,my_code: str):
                                      'to': 'Счет 11776614605963066702'}
 
 
-def test_filter_by_currency_not_key(test_transactions: list,my_code: str):
-    rub_transactions = filter_by_currency(test_transactions, my_code)
-    for _ in range(1):
-        assert next(rub_transactions) == {}
-
-
-def test_filter_by_currency_not_data(test_transactions: list,my_code_not: str):
-    rub_transactions = filter_by_currency(test_transactions, my_code_not)
-    for _ in range(1):
-        assert next(rub_transactions) == None
-
-
 #Тест функции transaction_descriptions
 
-@pytest.mark.parametrize("test_transactions, expected", [(test_transactions, "Перевод организации"),
-(test_transactions, "Перевод со счета на счет"),
-(test_transactions, "Перевод со счета на счет"),
-(test_transactions, "Перевод с карты на карту"),
-(test_transactions,"Перевод организации")])
 
-def test_transaction_descriptions(test_transactions, expected):
+def test_transaction_descriptions(test_transactions: list):
     descriptions = transaction_descriptions(test_transactions)
-    for _ in range(5):
-        assert next(descriptions) == expected
-
-
-def test_transaction_descriptions():
-    descriptions = transaction_descriptions()
-    for _ in range(5):
-        assert next(descriptions) == None
+    assert next(descriptions) == "Перевод организации"
+    assert next(descriptions) == "Перевод со счета на счет"
+    assert next(descriptions) == "Перевод со счета на счет"
+    assert next(descriptions) == "Перевод с карты на карту"
+    assert next(descriptions) == "Перевод организации"
 
 
 #Тест функции card_number_generator
+@pytest.fixture
+def my_code_start()-> int:
+    return 0
 
-@pytest.mark.parametrize("start, end, expected", [(1, 2, ("0000 0000 0000 0001" "0000 0000 0000 0002") ),
-                                                  (0,0, ("0000 0000 0000 0000"))])
+@pytest.fixture
+def my_code_end()-> int:
+    return 1
 
-def test_card_number_generator(start: int, end: int, expected: str):
-    assert card_number_generator(start, end) == expected
+
+def test_card_number_generator(my_code_start: int, my_code_end: int):
+    generator = card_number_generator(my_code_start, my_code_end)
+    assert next(generator) == "0000 0000 0000 0000"
+    assert next(generator) == "0000 0000 0000 0001"
