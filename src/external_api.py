862f1852fa64bc22129a16bcc94ce8b3b99d8037
Linar Khamidulin <hamidulin16@yandex.ru>
2024-10-09 19:41:30 +0300

    add external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -11,12 +11,31 @@ url = os.getenv('url')
 def currency_conversion_rubles_usd(sum_usd):
 
     headers ={'apikey' : api_key}
-    params = {'amount': sum_usd, 'from': 'USD', 'to': 'RUB'}
-
-    request = requests.get(url,  params=params, headers=headers)
+    params = {'to': 'RUB',  'from': 'USD', 'amount': sum_usd}
+    to = 'RUB'
+    from_ = 'USD'
+    amount = sum_usd
+    request = requests.get(url, headers=headers)
+    #request = requests.get(url,  params=params, headers=headers)
     request.raise_for_status()
     data = request.json()
     return round(data["result"], 2)
 
 def currency_conversion_rubles_eus():
     pass
+
+
+
+
+headers ={'apikey' : api_key}
+#params = {'to': 'RUB',  'from': 'USD', 'amount': sum_usd}
+to = 'RUB'
+from_ = 'USD'
+amount = 15
+request = requests.get(f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_}&amount={amount}", headers=headers)
+#request = requests.get(url,  params=params, headers=headers)
+request.raise_for_status()
+data = request.json()
+print(round(data["result"], 2))
+
+
