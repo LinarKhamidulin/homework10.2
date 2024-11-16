125689eb3db11968d5207ece100f5dbf553e3c10
Linar Khamidulin <hamidulin16@yandex.ru>
2024-11-16 13:26:33 +0300

    add file

--- main.py
+++ main.py
@@ -1,105 +1,11 @@
-import re
-from collections import Counter
-
-from isort.identify import imports
-from src.processing import filter_by_state, sort_by_date
-from src.utils_re import filter_parameters_from_the_user
+from src.utils_re import filter_parameters_from_the_user, counting_operations
 from src.widget import get_date, mask_account_card
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 from src.utils import open_file_json
 from src.handler_CSV_Excel import reading_a_file_csv, read_excel_file
 
-"""
-card_number = input("Наименование и номер карты: ")
-account_number = input("Номер счета: ")
-date_input = input("Дата: ")
-
-print(mask_account_card(card_number))
-print(mask_account_card(account_number))
-print(get_date(date_input))
-
-list_dictionary = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-state = input()
-if "" == "":
-    state = "EXECUTED"
-
-print(filter_by_state(list_dictionary, state))
-print(sort_by_date(list_dictionary))
-
-
-transactions = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    },
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    },
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160",
-    },
-    {
-        "id": 895315941,
-        "state": "EXECUTED",
-        "date": "2018-08-19T04:27:37.904916",
-        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод с карты на карту",
-        "from": "Visa Classic 6831982476737658",
-        "to": "Visa Platinum 8990922113665229",
-    },
-    {
-        "id": 594226727,
-        "state": "CANCELED",
-        "date": "2018-09-12T21:27:25.241689",
-        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод организации",
-        "from": "Visa Platinum 1246377376343588",
-        "to": "Счет 14211924144426031657",
-    },
-]
-print("11.1 Включения и генераторы")
-
-print("функция filter_by_currency")
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(2):
-    print(next(usd_transactions))
-
-print("функция transaction_descriptions")
-descriptions = transaction_descriptions(transactions)
-for _ in range(5):
-    print(next(descriptions))
-
-print("функция card_number_generator")
-for card_number in card_number_generator(1, 5):
-    print(card_number)
-"""
-
 
 def main() -> dict:
-
+    '''Главная функция принимает данные от пользователя для дальнейшей обработки'''
     print(
         """lower()
     Программа: Привет! Добро пожаловать в программу работы
@@ -118,6 +24,7 @@ def main() -> dict:
     filter_by_data = input("Отсортировать операции по дате? Да/Нет ")
     sort_in_ascending_or_descending_order = input("Отсортировать по возрастанию или по убыванию? ")
     filter_by_currency_ = input("Выводить только рублевые транзакции? Да/Нет ")
+    filter_by_word = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
     print(f"Распечатываю итоговый список транзакций для {name_user}")
 
     parameters_from_the_user = {
@@ -126,6 +33,7 @@ def main() -> dict:
         "data": filter_by_data,
         "ascending_or_descending": sort_in_ascending_or_descending_order,
         "currency": filter_by_currency_,
+        "words": filter_by_word
     }
     return parameters_from_the_user
 
@@ -136,11 +44,18 @@ def parameter_handler(parameters: dict)-> str:
 
         if parameters["process"] == "1":
             #обработки данных из файлов JSON
+
             file_open = open_file_json(parameters)
-            counted = 0
-            for i in file_open:
-                if i != {}:
-                    counted += 1
+            #выбор подсчета
+            #по операциям
+            if parameters["words"] == "да":
+                counted = counting_operations(file_open)
+            #все операции
+            elif parameters["words"] == "нет":
+                counted = 0
+                for i in file_open:
+                    if i != {}:
+                        counted += 1
 
             print(f"Всего банковских операций в выборке: {counted}")
 
@@ -166,10 +81,16 @@ def parameter_handler(parameters: dict)-> str:
         elif parameters["process"] == "2":
             #обработки данных из файлов CSV
             file_open = reading_a_file_csv(parameters)
-            counted = 0
-            for i in file_open:
-                if i != {}:
-                    counted += 1
+            #выбор подсчета
+            #по операциям
+            if parameters["words"] == "да":
+                counted = counting_operations(file_open)
+            #все операции
+            elif parameters["words"] == "нет":
+                counted = 0
+                for i in file_open:
+                    if i != {}:
+                        counted += 1
 
             print(f"Всего банковских операций в выборке: {counted}")
             for i in file_open:
@@ -193,10 +114,16 @@ def parameter_handler(parameters: dict)-> str:
         elif parameters["process"] == "3":
             #обработки данных из файлов XLSX
             file_open = read_excel_file(parameters)
-            counted = 0
-            for i in file_open:
-                if i != {}:
-                    counted += 1
+            #выбор подсчета
+            #по операциям
+            if parameters["words"] == "да":
+                counted = counting_operations(file_open)
+            #все операции
+            elif parameters["words"] == "нет":
+                counted = 0
+                for i in file_open:
+                    if i != {}:
+                        counted += 1
 
             print(f"Всего банковских операций в выборке: {counted}")
             for i in file_open:
@@ -219,7 +146,7 @@ def parameter_handler(parameters: dict)-> str:
         return f"ошибка TypeError"
 
     except Exception:
-        return f"ошибка"
+        return f"Не найдено ни одной транзакции, подходящей под ваши условия фильтрации"
 
 
 request_parameters = main()
--- src/utils_re.py
+++ src/utils_re.py
@@ -1,7 +1,9 @@
 import re
+from collections import Counter
 
 
 def filter_parameters_from_the_user(parameters: dict)-> dict:
+    '''функция для проверки ввода параметров для фильтрования'''
     try:
         file_to_process_ = re.findall(r"1|2|3", parameters.get("process"), flags=re.IGNORECASE)
         filter_by_status_ = re.findall(r"EXECUTED|CANCELED|PENDING", parameters.get("status"), flags=re.IGNORECASE)
@@ -10,6 +12,7 @@ def filter_parameters_from_the_user(parameters: dict)-> dict:
             r"по возрастанию|по убыванию", parameters.get("ascending_or_descending"), flags=re.IGNORECASE
         )
         filter_by_currency_ = re.findall(r"да|нет", parameters.get("currency"), flags=re.IGNORECASE)
+        filter_by_word_ = re.findall(r"да|нет", parameters.get("words"), flags=re.IGNORECASE)
 
         parameters_from_the_user = {
             "process": file_to_process_[0],
@@ -17,9 +20,21 @@ def filter_parameters_from_the_user(parameters: dict)-> dict:
             "data": filter_by_data_[0],
             "ascending_or_descending": sort_in_ascending_or_descending_order_[0],
             "currency": filter_by_currency_[0],
+            "words": filter_by_word_[0]
         }
 
         return parameters_from_the_user
 
     except Exception:
         return f"Указаны не верные параметры для сортировки {parameters}"
+
+
+def counting_operations(list_data: list)-> dict:
+    '''функция для фильтрования и подсчета операции'''
+    list_ = []
+    for dist in list_data:
+        description = re.findall("\D+", dist['description'], flags=0)
+        list_.append(*description)
+        counted = Counter(list_)
+
+    return counted
